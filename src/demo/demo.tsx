b76ced8cc885f6318aee3484f10877163187205c
alexperronnet <github.ayfb9@lxprnt.com>
2023-09-07 17:53:37 +0200

    feat: build sheet component

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -20,15 +20,28 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '@/lib/dialog'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/lib/sheet'
 import { Button } from '@/ui/button'
+
+const SHEET_SIDES = ['top', 'right', 'bottom', 'left'] as const
 
 export function Demo() {
   return (
-    <div className='flex min-h-screen items-center justify-center space-x-2 font-sans'>
+    <div className='container grid min-h-screen grid-cols-4 place-content-center gap-6 font-sans'>
       {/* Alert Dialog Component */}
       <AlertDialog>
         <AlertDialogTrigger asChild>
-          <Button variant='outline'>Alert Dialog Component</Button>
+          <Button className='col-span-2' variant='outline'>
+            Alert Dialog
+          </Button>
         </AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
@@ -47,7 +60,9 @@ export function Demo() {
       {/* Dialog Component */}
       <Dialog>
         <DialogTrigger asChild>
-          <Button variant='outline'>Dialog Component</Button>
+          <Button className='col-span-2' variant='outline'>
+            Dialog
+          </Button>
         </DialogTrigger>
         <DialogContent className='sm:max-w-[425px]'>
           <DialogHeader>
@@ -57,7 +72,7 @@ export function Demo() {
               content underneath inert.
             </DialogDescription>
           </DialogHeader>
-          <div className='grid gap-4 py-4'>
+          <div>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe minima error dicta
             accusantium alias odio fugit quo natus quae animi voluptatibus ipsa iste architecto enim
             sapiente, eveniet deserunt impedit rem.
@@ -65,6 +80,30 @@ export function Demo() {
           <DialogFooter>Dialog footer</DialogFooter>
         </DialogContent>
       </Dialog>
+
+      {/* Sheet Component */}
+      {SHEET_SIDES.map(side => (
+        <Sheet key={side}>
+          <SheetTrigger asChild>
+            <Button variant='outline'>Sheet {side}</Button>
+          </SheetTrigger>
+          <SheetContent side={side}>
+            <SheetHeader>
+              <SheetTitle>Sheet</SheetTitle>
+              <SheetDescription>
+                Extends the Dialog component to display content that complements the main content of
+                the screen.
+              </SheetDescription>
+            </SheetHeader>
+            <div className='py-4'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe minima error dicta
+              accusantium alias odio fugit quo natus quae animi voluptatibus ipsa iste architecto
+              enim sapiente, eveniet deserunt impedit rem.
+            </div>
+            <SheetFooter>Sheet footer</SheetFooter>
+          </SheetContent>
+        </Sheet>
+      ))}
     </div>
   )
 }
