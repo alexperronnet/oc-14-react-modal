dc3a8e6c9b243689f66484175ae2e8dcf370ad8f
alexperronnet <github.ayfb9@lxprnt.com>
2023-09-07 07:54:02 +0200

    feat: build alert-dialog component

--- src/demo/demo.tsx
+++ src/demo/demo.tsx
@@ -1,5 +1,39 @@
-import styles from './demo.module.css'
+import '@/lib/styles.css'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/lib'
+import { Button } from '@/ui/button'
 
 export function Demo() {
-  return <div className={styles.demo}>DemoPage</div>
+  return (
+    <div className='grid min-h-screen place-content-center font-sans'>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant='outline'>Alert Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  )
 }
