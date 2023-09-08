72d434c0912b1b7a88e2c1a39d95a596f9b5649a
alexperronnet <github.ayfb9@lxprnt.com>
2023-09-08 05:00:23 +0200

    fix: tying to fix github actions

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,5 @@
 const config = {
   '*.{mjs,cjs,js,jsx,ts,tsx}': ['eslint --fix'],
-  '!dist/**/*': [],
 }
 
 module.exports = config
--- .releaserc.cjs
+++ .releaserc.cjs
@@ -3,20 +3,14 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     [
       '@semantic-release/git',
       {
         assets: ['dist/**'],
-        message: 'chore(release): ${nextRelease.version} ${nextRelease.notes}',
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
     ],
     '@semantic-release/npm',
-    [
-      '@semantic-release/github',
-      {
-        assets: ['dist/**'],
-      },
-    ],
+    '@semantic-release/github',
   ],
 }
